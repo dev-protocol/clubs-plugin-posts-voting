989ee3a2efd675085c1edd2cd31e3464cf821a07
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2023-11-06 10:54:11 +0900

    Coding

--- src/components/feed-after-post-content.vue
+++ src/components/feed-after-post-content.vue
@@ -1,27 +1,38 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <template>
 	<section>
 		<div class="flex flex-col gap-2 mb-4">
 			<div class="relative flex justify-between">
 				<div class="pl-2 font-bold z-10">Nizi</div>
 				<div class="pr-2 font-bold z-10">60.9%</div>
-				<div class="absolute bg-blue-300 h-full rounded" style="width: 60.9%"></div>
+				<div
+					class="absolute bg-blue-300 h-full rounded"
+					style="width: 60.9%"
+				></div>
 			</div>
 			<div class="relative flex justify-between">
 				<div class="pl-2 font-bold z-10">SEVENTEEN</div>
 				<div class="pr-2 font-bold z-10">17.8%</div>
-				<div class="absolute bg-blue-300 h-full rounded" style="width: 17.8%"></div>
+				<div
+					class="absolute bg-blue-300 h-full rounded"
+					style="width: 17.8%"
+				></div>
 			</div>
 			<div class="relative flex justify-between">
 				<div class="pl-2 font-bold z-10">TAEMIN</div>
 				<div class="pr-2 font-bold z-10">17.5%</div>
-				<div class="absolute bg-blue-300 h-full rounded" style="width: 17.5%"></div>
+				<div
+					class="absolute bg-blue-300 h-full rounded"
+					style="width: 17.5%"
+				></div>
 			</div>
-				<div class="relative flex justify-between">
+			<div class="relative flex justify-between">
 				<div class="pl-2 font-bold z-10">JUNGKOOK</div>
 				<div class="pr-2 font-bold z-10">3.8%</div>
-				<div class="absolute bg-blue-300 h-full rounded" style="width: 3.8%"></div>
+				<div
+					class="absolute bg-blue-300 h-full rounded"
+					style="width: 3.8%"
+				></div>
 			</div>
 		</div>
 		<p>818 votes</p>
