90893bd000b863a05f5dadae0f59a05b829d2950
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2023-11-10 07:42:31 +0900

    make components

--- src/components/Voting/Result.vue
+++ src/components/Voting/Result.vue
@@ -25,14 +25,10 @@ const totalVotes = (
 const calculatePercent = (votes: number, total: number) => {
 	return Math.round((votes / total) * 100)
 }
-
 </script>
 <template>
 	<div class="flex flex-col gap-2 mb-4">
-		<div
-			v-for="option in vote.options"
-			class="relative flex justify-between"
-		>
+		<div v-for="option in vote.options" class="relative flex justify-between">
 			<div class="pl-2 flex items-center gap-1 z-10">
 				<div class="font-bold">{{ option.name }}</div>
 				<div v-if="option.id === vote.selected">✓</div>
@@ -43,9 +39,9 @@ const calculatePercent = (votes: number, total: number) => {
 			<div
 				class="absolute bg-blue-300 h-full rounded"
 				:style="`width: ${calculatePercent(
-							option.votes,
-							totalVotes(vote.options),
-						)}%`"
+					option.votes,
+					totalVotes(vote.options),
+				)}%`"
 			></div>
 		</div>
 	</div>
--- src/components/Voting/Vote.vue
+++ src/components/Voting/Vote.vue
@@ -14,17 +14,17 @@ const props = defineProps<{
 </script>
 <template>
 	<ul class="flex flex-col gap-1">
-	  <li v-for="option in vote.options" :key="option.id" class="list-none">
-		<button
-		  class="w-full text-blue-500 font-bold border border-blue-500 rounded-xl"
-		  @click="
-							() => {
-								handleClickVote(option.id)
-							}
-						"
-		>
-		  {{ option.name }}
-		</button>
-	  </li>
+		<li v-for="option in vote.options" :key="option.id" class="list-none">
+			<button
+				class="w-full text-blue-500 font-bold border border-blue-500 rounded-xl"
+				@click="
+					() => {
+						handleClickVote(option.id)
+					}
+				"
+			>
+				{{ option.name }}
+			</button>
+		</li>
 	</ul>
 </template>
