5ddbd8567059fd5db579c01bce3e6d93b1f659f1
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2023-12-04 12:25:01 +0900

    add connect button

---
+++ .preview/preview.ssr/src/theme/Connect.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import { BrowserProvider } from 'ethers'
+import { connection } from '@devprotocol/clubs-core/connection'
+
+const walletAddress = ref<string | undefined>('')
+
+const handleConnection = async () => {
+	const signer = connection().signer.value
+
+	console.log('signer', signer)
+
+	if (!signer) {
+		return
+	}
+
+	// get wallet address
+	const connectedAddress = await signer.getAddress()
+	walletAddress.value = connectedAddress
+
+	console.log('connectedAddress', connectedAddress)
+}
+
+onMounted(() => {
+	connection().signer.subscribe(handleConnection)
+})
+
+const onClick = async () => {
+	try {
+		const eth = (window as any).ethereum
+		await eth.send('eth_requestAccounts')
+		connection().setEip1193Provider(eth, BrowserProvider)
+	} catch (error) {
+		console.error(error)
+	}
+}
+</script>
+
+<template>
+	<div class="container mx-auto py-8">
+		<div class="flex justify-end">
+			<button
+				v-if="!walletAddress"
+				class="rounded-3xl border border-transparent bg-blue-600 px-8 py-2 text-base text-white shadow-sm focus:outline-none"
+				@click="onClick"
+			>
+				Wallet connect
+			</button>
+		</div>
+	</div>
+</template>
