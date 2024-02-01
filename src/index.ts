46bae9b4b7d5999302ac712ca3db506f389034a8
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2024-02-02 08:05:40 +0900

    Add autogenerated yarn.lock
    The autogenerated yarn.lock file has been added to the project. This file was created by the package manager to store exactly which versions of each dependency were installed.

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,9 @@ import { SlotName } from '@devprotocol/clubs-plugin-posts'
 import AfterContentForm from './components/edit-after-content-form.astro'
 import AfterPostContent from './components/feed-after-post-content.astro'
 import { votingHandler } from './ApiHandler.ts'
+import ImageVotingPreview01 from './assets/images/voting-preview01.png'
+import ImageVotingPreview02 from './assets/images/voting-preview02.png'
+import ImageVotingPreview03 from './assets/images/voting-preview03.png'
 
 export const getSlots = (async () => {
 	return [
@@ -24,10 +27,16 @@ export const getSlots = (async () => {
 	]
 }) satisfies ClubsFunctionGetSlots
 
-export const meta = {
+export const meta: ClubsPluginMeta = {
 	id: 'devprotocol:clubs:plugin:posts:voting',
 	displayName: 'Voting',
 	category: ClubsPluginCategory.Uncategorized,
+	description: 'Voting is a voting application that prioritizes ease of use. Users can create their own polls, allowing up to a maximum of four options. Once the voting period is over, the results can be immediately checked.',
+	previewImages: [
+		ImageVotingPreview01,
+		ImageVotingPreview02,
+		ImageVotingPreview03,
+	],
 	readme: Readme,
 } satisfies ClubsPluginMeta
 
