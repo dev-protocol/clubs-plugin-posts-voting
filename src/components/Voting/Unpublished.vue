1f27bdad82d9d28ec482b832744c3f0b21264854
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2024-04-11 14:58:56 +0900

    adds japanese localization support

--- src/components/Voting/Unpublished.vue
+++ src/components/Voting/Unpublished.vue
@@ -1,13 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { Strings } from '../i18n'
+import { i18nFactory } from '@devprotocol/clubs-core'
+
+const i18nBase = i18nFactory(Strings)
+let i18n = i18nBase(['en'])
+
+onMounted(() => {
+	i18n = i18nBase(navigator.languages)
+})
+</script>
 <template>
 	<div class="flex flex-col justify-center items-center p-4">
 		<div class="px-8 py-4 border border-gray-400 text-center">
 			<p class="text-xl text-gray-600 font-bold mb-2">
-				Poll Results are Unpublished
+				{{ i18n('Unpublished') }}
 			</p>
 			<p class="text text-gray-600">
-				At the moment, the poll results are set to unpublished.<br />Please wait
-				until the voting period has ended.
+				{{ i18n('UnpublishedDetail') }}<br />{{ i18n('PleaseWait') }}
 			</p>
 		</div>
 	</div>
---
+++ src/components/i18n/index.ts
@@ -0,0 +1,24 @@
+import type { ClubsI18nParts } from '@devprotocol/clubs-core'
+
+export const Strings = {
+	RemovePoll: {
+		en: 'Remove poll',
+		ja: '',
+	},
+	ImmediateResultPublicationSettings: {
+		en: 'Immediate Result Publication Settings',
+		ja: '',
+	},
+	Unpublished: {
+		en: 'Poll Results are Unpublished',
+		ja: '',
+	},
+	UnpublishedDetail: {
+		en: 'At the moment, the poll results are set to unpublished.',
+		ja: '',
+	},
+	PleaseWait: {
+		en: 'Please wait until the voting period has ended.',
+		ja: '',
+	},
+} satisfies ClubsI18nParts
