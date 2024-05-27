6278cba4b3a00f8488855705ce9ea84c6e0ca8d2
Yash Agrawal <yash@devprotocol.xyz>
2024-05-27 13:05:27 +0530

    feat: add localization

--- src/components/i18n/index.ts
+++ src/components/i18n/index.ts
@@ -3,11 +3,27 @@ import type { ClubsI18nParts } from '@devprotocol/clubs-core'
 export const Strings = {
 	RemovePoll: {
 		en: 'Remove poll',
-		ja: '投票を削除',
+		ja: '投票フォームをキャンセル',
+	},
+	PollLength: {
+		en: 'Poll length',
+		ja: '投票期間',
+	},
+	Days: {
+		en: 'Days',
+		ja: '日',
+	},
+	Hours: {
+		en: 'Hours',
+		ja: '時間',
+	},
+	Minutes: {
+		en: 'Minutes',
+		ja: '分',
 	},
 	ImmediateResultPublicationSettings: {
 		en: 'Immediate Result Publication Settings',
-		ja: '常に結果を表示する',
+		ja: '投票期間中に結果を表示する',
 	},
 	Unpublished: {
 		en: 'Poll Results are Unpublished',
@@ -20,5 +36,13 @@ export const Strings = {
 	PleaseWait: {
 		en: 'Please wait until the voting period has ended.',
 		ja: '投票期間の終了までお待ちください',
+	},
+	LimitedAccess: {
+		en: 'Limited access',
+		ja: '限定アクセス',
+	},
+	Post: {
+		en: 'Post',
+		ja: '投稿',
 	},
 } satisfies ClubsI18nParts
